a072131711964e7daef197b9926ca3fb983cf9e4
luozhixian <1597395958@qq.com>
2020-06-02 10:48:17 +0800

    updata

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+function say(arg: Number) {
+  console.log(arg, 'hello typeScript')
+}
+
+say(2)
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+'use strict'
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+module.exports = {
+  devtool: "cheap-module-eval-source-map",
+  devServer: {
+    contentBase: "./dist",
+    stats: "errors-only",
+    compress: false,
+    host: "localhost",
+    port: 8080
+  },
+
+  entry: './src/index.ts',
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.[hash:6].js',
+  },
+
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: 'index.html'
+    })
+  ],
+
+  module: {
+    rules: [{
+      test: /.tsx?$/,
+      use: "ts-loader",
+      exclude: /node_modules/
+    }]
+  }
+}
